7f653ac0a7a3db35d5e05c6df2d6feba6f7f2ac6
Даниил Федотов <fedotov_clever_91@mail.ru>
2022-09-30 15:31:16 +0300

    create shaders, buffers and draw a triangle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPosition;\n"
+"void main() \n"
+"{\n"
+"   gl_Position = vec4(aPosition.x, aPosition.y, aPosition.z, 1.0);\n"
+"}\n\0";
+
+// Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.08f, 0.13f, 0.4f, 1.0f);\n"
+"}\n\0";
+
 int main()
 {
     // Initialize GLFW
@@ -12,6 +28,13 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     // CORE profile (only have the modern functions)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLfloat vertices[] =
+    {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+         0.0f,  0.5f * float(sqrt(3)) / 2, 0.0f
+    };
 
     GLFWwindow* window = glfwCreateWindow(1024, 720, "Physical Pendulum", NULL, NULL);
     if (!window)
@@ -31,6 +54,47 @@ int main()
     // Specify the viewport of OpenGL in the window (from x,y to x1,y1)
     glViewport(0, 0, 1024, 720);
 
+    // Create Vertex Shader Object and get reference
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // Attach Vertex Shader source to the Vertex Shader Object
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    // Compile Vertex Shader into machine code
+    glCompileShader(vertexShader);
+
+    // Create Fragment Shader Object and get reference
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    // Attach Fragment Shader source to the Fragment Shader Object
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    // Compile Fragment Shader into machine code
+    glCompileShader(fragmentShader);
+
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
     // Specify color of the background, clean back buffer and assign new color to it
     glClearColor(0.08f, 0.13f, 0.4f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -40,9 +104,22 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.99f, 0.99f, 0.99f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+
         // All GLFW Events
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
 
